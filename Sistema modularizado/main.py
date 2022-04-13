4d2f27de44093df45f8eec1ec4be6feee8f0d94b
Gabriel Missima <92278167+Reh2g@users.noreply.github.com>
2022-04-12 21:23:53 -0300

    Update main.py

--- Sistema modularizado/main.py
+++ Sistema modularizado/main.py
@@ -1,4 +1,5 @@
 # Bibliotecas
+
 from time import sleep
 
 # Modulos
@@ -6,27 +7,33 @@ from Modules.Interface.introSystem import *
 from Modules.Archive.archive import *
 
 # PROGRAMA PRINCIPAL
+
 archive = 'users.txt'
 
 if not arquivoExiste(archive):
     criarArquivo(archive)
-
 
 while True:
     answer = menu(['Ver pessoas cadastradas',
     'Cadastrar nova pessoa',
     'Sair do programa'])
     if answer == 1:
-        # Opção de listar o conteúdo de um arquivo
+
+# Opção de listar o conteúdo de um arquivo
+
         lerArquivo(archive)
     elif answer == 2:
-        # Opção de cadastrar uma nova pessoa
+
+# Opção de cadastrar uma nova pessoa
+
         intro('NOVO CADASTRO')
         nome = str(input('Nome: '))
         idade = leiaInt('idade: ')
         cadastrar(archive, nome, idade)
     elif answer == 3:
-        # Opção de sair do sistema
+
+# Opção de sair do sistema
+
         intro('\033[1mSaindo do sistema... Até logo!\033[m')
         sleep(1)
         print('\033[1;36mUwU\033[m'.center(49))
